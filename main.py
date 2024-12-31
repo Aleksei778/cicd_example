9640d90d39269c8b956c081aea783a0077e9fad1
AlexPR <aleksejparhomenko14192@gmail.com>
2024-12-31 18:43:08 +0300

    add html

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from starlette.requests import Request
 
 
 app = FastAPI()
 
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
 def root():
     return {"msg": "Happy New Year!"}
 
+@app.get("/page", response_class=HTMLResponse)
+def page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "greeting": "Здарова, ублюдки!"})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
